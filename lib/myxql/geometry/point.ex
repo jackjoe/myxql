7f11aac3b91a34bfcd7740138633cf6d84047a7d
Jeroen Bourgois <jeroenbourgois@gmail.com>
2019-11-16 11:01:22 +0100

    chore: cleanup geometry modules

--- lib/myxql/geometry/point.ex
+++
@@ -1,8 +0,0 @@
-defmodule MyXQL.Geometry.Point do
-  @moduledoc """
-  Define the Point struct
-  """
-
-  @type t :: %MyXQL.Geometry.Point{coordinates: {number, number}, srid: non_neg_integer | nil}
-  defstruct coordinates: {0, 0}, srid: nil
-end
